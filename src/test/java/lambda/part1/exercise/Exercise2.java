e090abfc826293264da2e45f468199f8b25af10f
KashenkovSergej <kashenkovsergej@yandex.ru>
2021-01-10 17:57:13 +0300

    Exercise2 done

--- src/test/java/lambda/part1/exercise/Exercise2.java
+++ src/test/java/lambda/part1/exercise/Exercise2.java
@@ -35,14 +35,16 @@ class Exercise2 {
 
     @Test
     void implementsMultiplierUsingStatementLambda() {
-        Multiplier<Integer> multiplier = (value, multiplier1) -> value * multiplier1;
+        Multiplier<Integer> multiplier = (value, multiplier1) -> {
+            return value * multiplier1;
+        };
 
         testIntegerMultiplier(multiplier);
     }
 
     @Test
     void implementsIntegerMultiplierUsingExpressionLambda() {
-        Multiplier<Integer> multiplier = null;
+        Multiplier<Integer> multiplier = (value, multiplier1) -> value * multiplier1 ;
 
         testIntegerMultiplier(multiplier);
     }
@@ -86,7 +88,7 @@ class Exercise2 {
 
     @Test
     void implementsStringMultiplierUsingObjectMethodReference() {
-        Multiplier<String> multiplier = null;
+        Multiplier<String> multiplier = this::stringSumWithDelimiter;
 
         assertThat(multiplier.multiply("a", 3), is("a-a-a"));
         assertThat(multiplier.multiply("qwerty", 0), is(emptyString()));
